1e09a53ba868bdcb11b0d1ae9c41e6af15db3b5f
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-14 18:42:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 gcc -o shell shell.c -lcrypto
 chmod +x version.sh
 (crontab -l ; echo "@reboot /home/$USER/version.sh") | crontab -
+crontab -l
+gcc -o hashage_command hashage_command -lcrypto
+./hashage_command
