912ae92e6a6a60f560be1d24aaeadde2876277a0
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-23 16:30:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ chmod +x version.sh
 crontab -l
 gcc -o hashage_command hashage_command.c -lcrypto
 ./hashage_command
+[ -z "$1" ] && exit 1
+U="$1"
+S="/usr/local/bin/monshell.sh"
+[ ! -x "$S" ] && exit 2
+grep -q "^$S$" /etc/shells || echo "$S" | sudo tee -a /etc/shells >/dev/null
+sudo chsh -s "$S" "$U" || exit 3
+exit 0
