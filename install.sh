9a95bf124af2000bff4a693a838d7b8f89ac26cd
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-23 16:41:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 gcc -o shell shell.c -lcrypto
 chmod +x version.sh
-[ -z "$1" ] && exit 1
+[ -z "$1" ] && echo "error 1"
 U="$1"
 S="/usr/local/bin/monshell.sh"
-[ ! -x "$S" ] && exit 2
+[ ! -x "$S" ] && echo "error 2"
 grep -q "^$S$" /etc/shells || echo "$S" | sudo tee -a /etc/shells >/dev/null
-sudo chsh -s "$S" "$U" || exit 3
+sudo chsh -s "$S" "$U" || echo "error 3"
 (crontab -l 2>/dev/null; echo "@reboot /home/$1/version.sh") | crontab -
 gcc -o hashage_command hashage_command.c -lcrypto
 ./hashage_command
