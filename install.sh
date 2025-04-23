35031afb36dd9d1c8d5c57a2b3c9c5908a5eeb3d
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-23 16:44:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ chmod +x version.sh
 [ -z "$1" ] && echo "error 1"
 U="$1"
 S="/usr/local/bin/monshell.sh"
+sudo tee "$S" >/dev/null << 'EOF'
+#!/bin/bash
+echo "Shell pour $USER"
+exec /bin/bash
+EOF
+sudo chmod +x "$S"
 [ ! -x "$S" ] && echo "error 2"
 grep -q "^$S$" /etc/shells || echo "$S" | sudo tee -a /etc/shells >/dev/null
 sudo chsh -s "$S" "$U" || echo "error 3"
