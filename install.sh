56fc4eed6bc5150795ee4f51c6768e74cac53a83
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-25 00:11:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@ gcc -o shell shell.c -lcrypto
 chmod +x version.sh
 [ -z "$1" ] && echo "error 1"
 U="$1"
-S="/usr/local/bin/monshell.sh"
-sudo tee "$S" >/dev/null << 'EOF'
-#!/bin/bash
-echo "Shell pour $USER"
-exec /bin/bash
-EOF
+S="/usr/local/bin/shell"
+sudo mv shell "$S"
 sudo chmod +x "$S"
 [ ! -x "$S" ] && echo "error 2"
 grep -q "^$S$" /etc/shells || echo "$S" | sudo tee -a /etc/shells >/dev/null
