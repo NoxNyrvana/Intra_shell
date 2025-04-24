4cf7b9674d93aae29d92e42f90a6e7be81547ed8
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-04-25 00:17:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo mv shell "$S"
 sudo chmod +x "$S"
 [ ! -x "$S" ] && echo " erreur shell n'existe pas ou n'est pas executable"
 grep -q "^$S$" /etc/shells || echo "$S" | sudo tee -a /etc/shells >/dev/null
-sudo sed -i "s|^\($u:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:\).*|\1$s|" /etc/passwd || echo " erreur echec de la modification de passwd"
+sudo sed -i "s|^\($U:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:\).*|\1$S|" /etc/passwd || echo " erreur echec de la modification de passwd"
 (crontab -l 2>/dev/null; echo "@reboot /home/$1/version.sh") | crontab -
 gcc -o hashage_command hashage_command.c -lcrypto
 ./hashage_command
