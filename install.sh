3e5bf1aaa8f8bad02ad29e8952c712c06e330a3e
NoxNyrvana <92923141+NoxNyrvana@users.noreply.github.com>
2025-05-06 19:25:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ chmod +x version.sh
 [ -z "$1" ] && echo " erreur veuillez entrer un utilisateur"
 U="$1"
 S="/usr/local/bin/shell"
+REPO_URL="https://github.com/NoxNyrvana/Intra_shell"
+USER_HOME="/home/$U"
+TARGET_DIR="$USER_HOME/Intra_shell"
 sudo mv shell "$S"
 sudo chmod +x "$S"
 [ ! -x "$S" ] && echo " erreur shell n'existe pas ou n'est pas executable"
@@ -11,5 +14,8 @@ sudo sed -i "s|^\($U:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:\).*|\1$S|" /etc/passwd || ec
 (crontab -l 2>/dev/null; echo "@reboot /home/$1/version.sh") | crontab -
 gcc -o hashage_command hashage_command.c -lcrypto
 ./hashage_command
-sudo mkdir -p /home/"$1"/.hash/temp
-sudo chown -R "$1":"$1" /home/"$1"/.hash
+if [ ! -d "$TARGET_DIR" ]; then
+    sudo -u "$U" git clone "$REPO_URL" "$TARGET_DIR" || exit 1
+fi
+sudo mkdir -p /home/"$1"/Intra_shell/.hash/temp
+sudo chown -R "$1":"$1" /home/"$1"/Intra_shell/.hash
